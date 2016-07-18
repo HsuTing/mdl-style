ce7495d99ffa2a9293dc6356576b0ee5d89fe7e8
HsuTing <hsuting0106@gmail.com>
2016-07-19 04:19:34 +0800

    add state and media

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mdl-style
 
 Here are [material design lite](https://getmdl.io/) style for [radium](https://github.com/FormidableLabs/radium).
-
+All style are in [here](./style)
--- src/change.jsx
+++ src/change.jsx
@@ -8,6 +8,7 @@ import MDL from './../mdl';
 
 let output = {};
 
+
 //spite ','
 Object.keys(MDL).map((key) => {
   key.split(',').map((className) => {
@@ -22,6 +23,7 @@ Object.keys(MDL).map((key) => {
     }
   });
 });
+
 
 //add media
 delete output.mediaQueries;
@@ -42,12 +44,45 @@ Object.keys(MDL.mediaQueries).map((media) => {
 
         else {
           let temp = Merge({}, output[ className ][  ('@media ' + media) ], MDL.mediaQueries[media][ key ]);
-          delete output[className][ ('@media ' + media) ];
-          output[className][ ('@media ' + media) ] = temp;
+          delete output[ className ][ ('@media ' + media) ];
+          output[ className ][ ('@media ' + media) ] = temp;
         }
       }
     });
   });
+});
+
+
+//hover, focus, active
+let tempOutput = Merge({}, output);
+
+const addState = (key, state) => {
+  if(key.indexOf(state) !== -1) {
+    delete output[key];
+
+    let className = key.replace(state, '');
+    if(output[ className ] === undefined) {
+      let temp = {};
+      temp[ state ] = tempOutput[key];
+      output[ className ] = Merge({}, temp);
+    }
+
+    else {
+      if(output[ className ][ state ] === undefined) {
+        output[ className ][ state ] = tempOutput[key]
+      }
+    }
+  }
+};
+
+Object.keys(tempOutput).map((key) => {
+  [':hover', ':focus', ':active'].map((state) => {
+    addState(key, state);
+  });
+});
+
+Object.keys(output).map((key) => {
+  console.log(key);
 });
 
 let text = 'module.exports = ';
@@ -55,8 +90,4 @@ text += JSON.stringify(output);
 
 fs.writeFile(path.resolve(__filename, './../../index.js'), text, (err) => {
   if (err) throw err;
-
-  console.log();
-  console.log('It\'s ok.');
-  console.log();
 });
